54fce2e2a1c41e3811f9ea95a0e7b034f334eda5
龔彥賓 <Ben@gongyanbinde-MacBook-Pro.local>
2017-05-31 21:40:19 +0800

    side nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import {MdButtonModule, MdCheckboxModule,MdSidenavModule} from '@angular/material';
+import 'hammerjs';
+import { ButtonComponent } from './button/button.component';
+import {MdTabsModule} from '@angular/material';
+import { SidenavComponent } from './sidenav/sidenav.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdTabsModule,
+    MdSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
