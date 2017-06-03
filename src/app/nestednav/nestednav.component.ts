6728d61660f24ccbfce5c93f277aaed07d5380dc
龔彥賓 <Ben@gongyanbinde-MacBook-Pro.local>
2017-06-03 23:05:46 +0800

    加入註解

--- src/app/nestednav/nestednav.component.ts
+++ src/app/nestednav/nestednav.component.ts
@@ -19,16 +19,20 @@ export class NestednavComponent implements OnInit {
     if(this.SelectedEntity==Entity){
       this.SelectedEntity=null
       Entity.Selected=false;
+      //遞迴關閉與主menu節點相關的子menu節點
       if(Entity.ParentID==null){
        for(let i=0;i<this.menudata.length;i++){
+         if(Entity.ID==this.menudata[i].Entity.ID){
          if(this.menudata[i].ChildNodes.length!=0){
-           for(let j =0 ;j<this.menudata[i].ChildNodes.length;j++){
-             console.log(this.menudata[i].ChildNodes[j].Entity.Text)
-             this.menudata[i].ChildNodes[j].Entity.Selected=false;
-           this.Recurisve(this.menudata[i].ChildNodes[j])
-           }
+                   for(let j =0 ;j<this.menudata[i].ChildNodes.length;j++){
+                     console.log(this.menudata[i].ChildNodes[j].Entity.Text)
+                     this.menudata[i].ChildNodes[j].Entity.Selected=false;
+                   this.Recurisve(this.menudata[i].ChildNodes[j])
+                   }
 
          }
+         }
+
        }
       }
       console.log("close")
@@ -40,8 +44,8 @@ export class NestednavComponent implements OnInit {
   }
     console.log(Entity)
   }
+  //遞回關閉menu子節點
   Recurisve(data){
-
     if(data.length!=0){
            for(let j =0 ;j<data.ChildNodes.length;j++){
             console.log(data.ChildNodes[j].Entity.Text)
@@ -49,5 +53,4 @@ export class NestednavComponent implements OnInit {
            }
         }
   }
-
 }
