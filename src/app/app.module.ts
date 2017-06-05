9bfa280a1a73a87662362f114d528f9d35ec7808
Kung Ben <Kung Ben>
2017-06-05 10:12:54 +0800

    add app-container css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import {MdButtonModule, MdCheckboxModule,MdSidenavModule,MdListModule,MdCardModule} from '@angular/material';
+import {
+  // MdButtonModule, MdCheckboxModule,
+  // MdSidenavModule,MdListModule,MdCardModule,
+  // MdIconModule,MdTabsModule,
+  // MdSidenavContainer, MdToolbarModule,
+  MaterialModule
+} from '@angular/material';
 import 'hammerjs';
 import { ButtonComponent } from './button/button.component';
-import { MdTabsModule, MdSidenavContainer, MdToolbarModule } from '@angular/material';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { NestednavComponent } from './nestednav/nestednav.component';
 import { AppRoutingModule } from "./app-routing.module";
@@ -26,13 +31,15 @@ import { HomeComponent } from './home/home.component';
     FormsModule,
     HttpModule,
       AppRoutingModule,
-    MdButtonModule,
-    MdCheckboxModule,
-    MdTabsModule,
-    MdSidenavModule,
-    MdListModule,
-    MdCardModule,
-    MdToolbarModule
+      MaterialModule,
+    // MdButtonModule,
+    // MdCheckboxModule,
+    // MdTabsModule,
+    // MdSidenavModule,
+    // MdListModule,
+    // MdCardModule,
+    // MdToolbarModule,
+    // MdIconModule
 
   ],
   providers: [],
