b5c3f5be9bf6358da1bb0a55d00f3ceed16119dc
龔彥賓 <Ben@gongyanbinde-MacBook-Pro.local>
2017-06-08 14:29:26 +0800

    oo

--- src/app/form-meta/form-metaform/form-metaform.component.ts
+++ src/app/form-meta/form-metaform/form-metaform.component.ts
@@ -19,73 +19,66 @@ export class FormMetaformComponent implements OnInit {
   OptionsArray = [""];
   formoptionSettingType = "";//目前所要新增的表單選項
   PreviewForm;//預覽表單外觀
-  ShowPreviewForm:boolean
+  ShowPreviewForm: boolean
   constructor(private service: MetaformService, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.service.GetMetaformData().subscribe(res => {
       this.MetaformData = res;
-
       this.MetaformDataArray.push(res);
-
       console.log(this.MetaformDataArray[0][4][0].OptionsData)
       this.form = this.fb.group({
         title: ["", [Validators.required]],
+        content: ["", [Validators.required]],
         formoptions: this.fb.array([
-        this.FormOptionsGroup()
-
+          this.FormOptionsGroup()
         ])
-
       })
-
       this.showform = true;
-
     })
   }
   onAdd() {
     if (this.form['controls']['formoptions']['controls'][this.MetaformDataArray.length - 1].pristine) {
       confirm("表單選項沒有修改")
     } else {
-
-      const formoptionsarray = (this.form.controls.formoptions as FormArray)
+     const formoptionsarray = (this.form.controls.formoptions as FormArray)
       formoptionsarray.push(
         this.FormOptionsGroup()
       );
-
       this.MetaformDataArray.push(this.MetaformData)
 
 
     }
   }
-  FormOptionsGroup(){
+  FormOptionsGroup() {
     return new FormGroup({
-          formoption: new FormControl(null, [Validators.required]),
-          formoptionName: new FormControl(null, [Validators.required]),
-          formoptionSetting: new FormGroup({
-            InputType: new FormControl("", [Validators.required]),
-            isRequiredInput: new FormControl("", [Validators.required]),
-            Options: this.fb.array([
-              this.MetaformDataArray[0][4][0].OptionsData.map(o => (this.fb.control(o, [Validators.required])))
-            ]),
-            isMultiLine: new FormControl("", [Validators.required]),
-              isMultiSelect: new FormControl("", [Validators.required]),
-          })
-        })
+      formoption: new FormControl(null, [Validators.required]),
+      formoptionName: new FormControl(null, [Validators.required]),
+      formoptionSetting: new FormGroup({
+        InputType: new FormControl("", [Validators.required]),
+        isRequiredInput: new FormControl("", [Validators.required]),
+        Options: this.fb.array([
+          this.MetaformDataArray[0][4][0].OptionsData.map(o => (this.fb.control(o, [Validators.required])))
+        ]),
+        isMultiLine: new FormControl("", [Validators.required]),
+        isMultiSelect: new FormControl("", [Validators.required]),
+      })
+    })
   }
   onChange(i) {
     const formoption = this.form['controls']['formoptions']['controls'][i]['controls']['formoption'].value
     if (formoption == 'input') {
       (this.form['controls']['formoptions']['controls'][i]['controls']['formoptionSetting']['controls']['Options']).disable();
       (this.form['controls']['formoptions']['controls'][i]['controls']['formoptionSetting']['controls']['isMultiSelect']).disable();
-       (this.form['controls']['formoptions']['controls'][i]['controls']['formoptionSetting']['controls']['InputType']).enable();
-        this.form['controls']['formoptions']['controls'][i]['controls']['formoptionSetting']['controls']['isMultiLine'].enable();
+      (this.form['controls']['formoptions']['controls'][i]['controls']['formoptionSetting']['controls']['InputType']).enable();
+      this.form['controls']['formoptions']['controls'][i]['controls']['formoptionSetting']['controls']['isMultiLine'].enable();
 
     }
     else {
       if (formoption != 'select') {
         (this.form['controls']['formoptions']['controls'][i]['controls']['formoptionSetting']['controls']['isMultiSelect']).disable();
-      }else{
-           (this.form['controls']['formoptions']['controls'][i]['controls']['formoptionSetting']['controls']['isMultiSelect']).enable();
+      } else {
+        (this.form['controls']['formoptions']['controls'][i]['controls']['formoptionSetting']['controls']['isMultiSelect']).enable();
       }
       (this.form['controls']['formoptions']['controls'][i]['controls']['formoptionSetting']['controls']['Options']).enable();
       this.form['controls']['formoptions']['controls'][i]['controls']['formoptionSetting']['controls']['InputType'].disable();
@@ -114,9 +107,9 @@ export class FormMetaformComponent implements OnInit {
 
 
   }
-  isPreviewForm(e){
-this.ShowPreviewForm=e.checked
-console.log(this.ShowPreviewForm)
+  isPreviewForm(e) {
+    this.ShowPreviewForm = e.checked
+    console.log(this.ShowPreviewForm)
   }
   onSubmit() {
     console.log(this.form)
