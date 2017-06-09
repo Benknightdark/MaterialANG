e9d5e9790dee9a544387a12faf2340f70df117f9
Kung Ben <Kung Ben>
2017-06-09 11:23:07 +0800

    ol

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   firebase: {
 apiKey: 'AIzaSyCYUgJZKbpDDmQZMN9PcEXFruEriWpv_Kw',
 authDomain: 'helloeightd.firebaseapp.com',
-databaseURL: 'https://helloeightd.firebaseio.com/ ',
+databaseURL: 'https://helloeightd.firebaseio.com',
 projectId: 'helloeightd',
 storageBucket: 'gs://helloeightd.appspot.com',
 messagingSenderId: '859667729843 '
