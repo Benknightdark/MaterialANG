60c3447b1c43f98309f1abf2a5c5873f2fed488c
龔彥賓 <Ben@gongyanbinde-MacBook-Pro.local>
2017-06-10 13:31:05 +0800

    OK

--- src/app/form-meta/templateformmeta/templateformmeta.component.ts
+++ src/app/form-meta/templateformmeta/templateformmeta.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-templateformmeta',
@@ -33,7 +33,7 @@ export class TemplateformmetaComponent implements OnInit {
     return {
       FormOptionType: "",
       FormOptionName: "",
-      InputOption: "",
+      InputType: "",
       Required: "",
       isMulti: "",
     }
